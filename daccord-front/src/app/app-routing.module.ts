faa193d48d82501c623f5796756eb457195bf9f0
DIR\shayane.n.da.silva <shayane.n.da.silva@accenture.com>
2021-11-19 23:09:24 -0300

    Create lesson-create componet's  topbar

--- daccord-front/src/app/app-routing.module.ts
+++ daccord-front/src/app/app-routing.module.ts
@@ -1,16 +1,21 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { LessonComponent } from './views/lesson/lesson.component';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { LessonComponent } from "./views/lesson/lesson.component";
+import { LessonCreateComponent } from "./views/lesson/lesson-create/lesson-create.component";
 
 const routes: Routes = [
-    {
-        path: "lesson",
-        component: LessonComponent
-    }
+  {
+    path: "lesson",
+    component: LessonComponent,
+  },
+  {
+    path: "lesson/create",
+    component: LessonCreateComponent,
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ daccord-front/src/app/views/lesson/lesson-create/lesson-create.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lesson-create',
+  templateUrl: './lesson-create.component.html',
+  styleUrls: ['./lesson-create.component.css']
+})
+export class LessonCreateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
