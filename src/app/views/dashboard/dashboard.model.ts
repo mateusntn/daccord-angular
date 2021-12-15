5d9b8702329cc60bea08f87684079b90a3aa6c8a
mateusntn <mateusantoniodev@gmail.com>
2021-12-15 02:01:44 -0300

    Create dashboard model

---
+++ src/app/views/dashboard/dashboard.model.ts
@@ -0,0 +1,12 @@
+export interface Dashboard {
+    lessonsQuantity: number;
+    violaoQuantity: number;
+    violinoQuantity: number;
+    pianoQuantity: number;
+    saxofoneQuantity: number;
+    cavaquinhoQuantity: number;
+    easyQuantity: number;
+    mediumQuantity: number;
+    hardQuantity: number;
+    lastLessons: string[];
+}
